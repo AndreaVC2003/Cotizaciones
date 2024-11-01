0f0d13f155df2ee48b90f0aca0631e7649179aca
PabloAG11 <pablo11almaguer11@gmail.com>
2024-11-01 01:08:14 -0600

    Estructura Cotizacion.java

--- Cotizacion.java
+++ Cotizacion.java
@@ -3,7 +3,8 @@ package cotizacion;
 
 public class Cotizacion {
     public static void main(String[] args) {
-
+        Modelo m = new Modelo();
+        Vista v = new Vista(m);
+        Controlador c = new Controlador(m, v);
     }
-    
 }
